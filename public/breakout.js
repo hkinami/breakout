af77e35d676b1beba87053a07ee7495c074a602c
Hideo Kinami <hideo.kinami@compass-e.com>
2018-09-30 16:20:33 +0900

    First commit

---
+++ public/breakout.js
@@ -0,0 +1,355 @@
+
+class Container {
+    constructor(element) {
+        this.container = element
+        this.bricks = []
+    }
+
+    reset() {
+        for( let blick of this.bricks) {
+            this.container.removeChild(blick.element())
+        }
+        this.bricks = []
+        let messages = document.getElementsByClassName('message');
+        for(let i=0; i<messages.length; ++i) {
+            this.container.removeChild(messages.item(i))
+        }
+    }
+
+    rect() {
+        return this.container.getBoundingClientRect()
+    }
+
+    isCollide(ball) {
+        const rec = this.rect()
+        if ( ball.getOffetLeft() <= 0 ) {
+            return "left"
+        } else if ( rec.width <= ball.getOffetLeft()) {
+            return "right"
+        } else if ( ball.getOffetTop() <= 0 ) {
+            return "top"
+        } else if ( rec.height <= ball.getOffetTop()) {
+            return "bottom"
+        }
+        return ""
+    }
+
+    append(brick) {
+        this.bricks.push(brick)
+        this.container.appendChild(brick.element())
+    }
+
+    removeHitBlock(ball) {
+        const index = this.bricks.findIndex( (brick) => brick.isCollide(ball) !== 0 )
+        if (index >= 0) {
+            const brick = this.bricks[index]
+            brick.remove()
+            this.bricks.splice(index, 1)
+            return brick
+        }
+        return null
+    }
+
+    numOfBlocks() {
+        return this.bricks.length
+    }
+
+    gameOver() {
+        let message = document.createElement("div");
+        message.setAttribute('class', 'message');
+        message.innerHTML = "Game Over"
+        this.container.appendChild(message)
+    }
+
+    complete() {
+        let message = document.createElement("div");
+        message.setAttribute('class', 'message');
+        message.innerHTML = "Complete"
+        this.container.appendChild(message)
+    }
+
+}
+
+class Paddle {
+    constructor(element) {
+        this.paddle = element
+        this.direction = 'stop'
+    }
+
+    keyDown(event) {
+        if (event.key === "ArrowRight") {
+            this.direction = "right"
+        } else if (event.key === "ArrowLeft") {
+            this.direction = "left"
+        }
+    }
+
+    keyUp(event) {
+        if (event.key === "ArrowRight" || event.key === "ArrowLeft") {
+            this.direction = "stop"
+        }  
+    }
+
+    move() {
+        let left = this.paddle.offsetLeft;
+        if (this.direction === "left") {
+            left -= 5;
+        } else if (this.direction === "right") {
+            left += 5;
+        }
+        this.paddle.style.left = left + 'px';
+    }
+
+    rect() {
+        return this.paddle.getBoundingClientRect()
+    }
+
+    isCollide(ball) {
+        const rect = this.rect()
+        var rectBall = ball.rect();
+        
+        if ( !(
+            rect.bottom < rectBall.top ||
+            rect.top > rectBall.bottom ||
+            rect.right < rectBall.left ||
+            rect.left > rectBall.right
+        )) {
+            return ((rectBall.x - rect.x) / rect.width) * 10 - 5
+        }
+        return 0
+    }
+}
+
+
+class Ball {
+    constructor(element) {
+        this.ball = element
+        this.reset()
+    }
+
+    reset() {
+        this.direction = {
+            x: 0,
+            y: 5
+        }
+        this.ball.style.left = '50%'
+        this.ball.style.top = '50%'
+    }
+
+    move() {
+        const x = this.ball.offsetLeft + this.direction.x
+        const y = this.ball.offsetTop + this.direction.y
+
+        this.ball.style.left = x + 'px';
+        this.ball.style.top = y + 'px';
+    }
+
+    setX(dir) {
+        this.direction.x = dir
+    }
+
+    turnX() {
+        this.direction.x *= -1
+    }
+
+    turnY() {
+        this.direction.y *= -1
+    }
+
+    rect() {
+        return this.ball.getBoundingClientRect()
+    }
+
+    getOffetLeft() {
+        return this.ball.offsetLeft
+    }
+
+    getOffetTop() {
+        return this.ball.offsetTop
+    }
+}
+
+class Brick {
+    constructor(x, y, points) {
+        this.brick = document.createElement("div");
+        this.brick.setAttribute('class', 'brick');
+        this.brick.style.left = x + 'px';
+        this.brick.style.top = y + 'px';
+        this.brick.dataset.points = points;
+        this.brick.innerHTML = points;
+    }
+
+    element() {
+        return this.brick
+    }
+
+    points() {
+        return parseInt(this.brick.dataset.points)
+    }
+
+    rect() {
+        return this.brick.getBoundingClientRect()
+    }
+
+    remove() {
+        this.brick.parentNode.removeChild(this.brick)
+    }
+
+    isCollide(ball) {
+        const rect = this.rect()
+        var rectBall = ball.rect();
+        
+        if ( !(
+            rect.bottom < rectBall.top ||
+            rect.top > rectBall.bottom ||
+            rect.right < rectBall.left ||
+            rect.left > rectBall.right
+        )) {
+            return ((rectBall.x - rect.x) / rect.width) * 10 - 5
+        }
+        return 0
+    }
+}
+
+class BreakOut {
+    constructor() {
+        this.container = new Container(document.querySelector('#container'))
+        this.paddle = new Paddle(document.querySelector('#paddle'))
+        this.ball = new Ball(document.querySelector('#ball'))
+        this.button = document.querySelector("#start");
+        this.livesSpan = document.querySelector("#lives");
+        this.pointsSpan = document.querySelector("#points");
+
+        document.addEventListener("keydown", this.keyDown.bind(this) )
+        document.addEventListener("keyup", this.keyUp.bind(this) )
+        this.button.addEventListener("click", this.start.bind(this))
+
+        this.animation = null
+    }
+
+    reset() {
+        if (this.animation) {
+            cancelAnimationFrame(this.animation)
+        }
+        this.animation = null
+        this.button.innerHTML = "Start"
+        this.lives = 3
+        this.livesSpan.innerHTML = this.lives
+        this.points = 0
+        this.pointsSpan.innerHTML = this.points
+        this.ball.reset()
+        this.container.reset()
+        this.setupBricks(3)
+    }
+
+    setupBricks(numRow) {
+        const width = this.container.rect().width
+        for(let row=0; row < numRow; ++row) {
+            
+            let x = width % 100 / 2
+            let y = row * 70
+            
+            while( x < width - 100 ) {
+                let point = Math.ceil(Math.random() * 10 + 2)
+                let brick = new Brick(x, y, point)
+                this.container.append(brick)
+                x += 100
+            }
+        }
+        
+    }
+
+    start() {
+        if (this.button.innerHTML === "Restart") {
+            this.reset()
+            this.button.innerHTML = "Start"
+        } else if (this.animation === null) {
+            this.button.innerHTML = "Pause"
+            this.animation = requestAnimationFrame(this.update.bind(this));
+        } else {
+            cancelAnimationFrame(this.animation);
+            this.animation = null;
+            this.button.innerHTML = "Start"
+        }
+    }
+
+    keyUp(e) {
+        e.preventDefault()
+        this.paddle.keyUp(e)
+        if (this.animation === null) {
+            this.start()
+        }
+    }
+
+    keyDown(e) {
+        e.preventDefault()
+        this.paddle.keyDown(e)
+    }
+
+    bounceBall() {
+        const bounce = this.container.isCollide(this.ball)
+        if ( bounce === "right" || bounce === "left" ) {
+            this.ball.turnX()
+        } else if ( bounce === "top") {
+            this.ball.turnY()
+        } else if (bounce === "bottom") {
+            this.ball.turnY()
+            return false
+        }
+    
+        const hitPosion = this.paddle.isCollide(this.ball)
+        if ( hitPosion != 0 ) {
+            this.ball.setX(hitPosion)
+            this.ball.turnY()
+        }
+        return true
+    }
+
+    hitBlock() {
+        const brick = this.container.removeHitBlock(this.ball)
+        if (brick !== null) {
+            this.ball.turnY()
+            return brick
+        }
+        return null
+    }
+
+    countup(brick) {
+        this.points += brick.points()
+        this.pointsSpan.innerHTML = this.points
+    }
+
+    update() {
+        this.paddle.move()
+        this.ball.move()
+        
+        const brick = this.hitBlock()
+        if (brick) {
+            this.countup(brick)
+        } else {
+            if (! this.bounceBall()) {
+                -- this.lives
+                this.livesSpan.innerHTML = this.lives
+                if ( this.lives <= 0 ) {
+                    this.container.gameOver()
+                    this.button.innerHTML = "Restart"
+                } else {
+                    this.animation = null;
+                    this.button.innerHTML = "Start"
+                    this.ball.reset()
+                }
+                return
+            }
+        }
+
+        if (this.container.numOfBlocks() === 0) {
+            this.container.complete()
+            this.button.innerHTML = "Restart"
+        } else {
+            this.animation = requestAnimationFrame(this.update.bind(this));
+        }
+    }
+}
+
+const brakout = new BreakOut()
+brakout.reset()
